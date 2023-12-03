0769b5cbb108e2fe700af719a571b38f3b1f96e3
Tiple <tipleshashank@gmail.com>
2023-12-03 16:02:05 -0600

    update1

---
+++ node-mongo/index.js
@@ -0,0 +1,16 @@
+import { express } from "express";
+import { routes } from './src/routes/crmRoutes';
+
+const express = require('express');
+var app = express();
+const PORT= 3000;
+
+routes(app);
+
+app.get('/', (req, res)=>
+    res.send(`Node and express server is running on port${PORT}`)
+);
+
+app.listen(PORT, ()=>
+    console.log(`Your server is running on port ${PORT}`)
+);
